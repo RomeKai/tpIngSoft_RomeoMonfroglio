4390ad1964c410a7a362f69109918a569f5e843e
Romeo L. Monfroglio <141967584+RomeKai@users.noreply.github.com>
2025-04-13 17:56:45 -0300

    [Parte 2] Models

--- tpIngSoft/config/settings.py
+++ tpIngSoft/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
+    'config',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'tpIngSoft.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'tpIngSoft.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
